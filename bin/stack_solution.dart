aa482ddbe2a5c217d0377cf83812636fa51cd133
EhisEA <emmanueleayemere@gmail.com>
2022-04-20 15:57:43 +0100

    initial

---
+++ bin/stack_solution.dart
@@ -0,0 +1,5 @@
+import 'package:stack_solution/stack_solution.dart' as stack_solution;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stack_solution.calculate()}!');
+}
---
+++ test/stack_solution_test.dart
@@ -0,0 +1,8 @@
+import 'package:stack_solution/stack_solution.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
